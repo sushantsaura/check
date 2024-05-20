344db0ba6abd4792ace75929a3cafa8d7ea3e49a
sushantsaura <sauravsusahnt58@gmail.com>
2024-05-20 15:38:35 +0530

    Example

--- app/src/main/java/com/example/learn_modifiers/MainActivity.kt
+++ app/src/main/java/com/example/learn_modifiers/MainActivity.kt
@@ -25,7 +25,10 @@ class MainActivity : ComponentActivity() {
                   modifier = Modifier.fillMaxSize(),
                   color = MaterialTheme.colorScheme.background
               ) {
-                  TestModifiers()
+                 // TestLayout()
+                  //TestModifiers()
+                  TestExample()
+
               }
             }
         }
